92220efd80fef8ea465610ccafdc4987cfd85f25
Readiz <readiz@readiz.com>
2018-05-07 14:09:37 +0900

    Change platform

--- old_terminal/js/readiz_util.js
+++ old_terminal/js/readiz_util.js
@@ -95,71 +95,63 @@ var createElement = (function()
 })();
 
 var Readiz = {};
-Readiz.terminalSettingsObject = { 
-    ps: '[user@readiz]$', 
-    i18n: {
-        welcome: '#######################################\n' +
-            '# Readiz\n' +
-            '# -\n' +
-            '# Read everything in the world easily.\n' +
-            '# <a href="http://readiz.com/">http://readiz.com/</a>\n' +
-            '########################################\n\n' +
-            'Executable Commands:\nhelp, hello, history, clear\n' +
-            'main, about, log, blog, util, feed, bot, \nstock, memo, collected, plogin\n'
-    }
-};
-Readiz.makePublicPageObject = function(keyword) {
-    return {
-        name : keyword,
-        method : function(cmd){
-            cmd.out = 'Redirecting...';
-            location.href = 'http://' + keyword + '.readiz.com/';
-            return cmd;
-        },
-        help : 'Goto ' + keyword + ' page.'
-    }
-};
-Readiz.makePrivatePageObject = function(keyword) {
-    return {
-        name : keyword,
-        method : function(cmd){
-            cmd.out = 'Redirecting...';
-            location.href = 'https://p.readiz.com/' + keyword;
-            return cmd;
-        },
-        help : 'Goto ' + keyword + ' page.'
-    }
-};
-Readiz.terminalCommandsObject = [{
-    name : 'hello',
-    method : function(cmd){
-        cmd.out = '<h1>Hello World!</h1>';
-        return cmd;
+Readiz.terminalCommandsObject = {
+    help: function() {
+        this.echo('help, main, about, log, blog, util, feed, bot, stock, memo, echo');
     },
-    help : 'A demo command.'
-},
-Readiz.makePublicPageObject('main'),
-Readiz.makePublicPageObject('about'),
-Readiz.makePublicPageObject('log'),
-Readiz.makePublicPageObject('blog'),
-Readiz.makePublicPageObject('util'),
-Readiz.makePublicPageObject('feed'),
-Readiz.makePublicPageObject('bot'),
-Readiz.makePrivatePageObject('stock'),
-Readiz.makePrivatePageObject('memo'),
-Readiz.makePrivatePageObject('collected'),
-{
-    name : 'plogin',
-    method : function(cmd) {
-        var last = $ptty.get_command_option('last');
-        var args = last.split(' ');
-        var arg1 = args[1];
-        var arg2 = args[2];
-
-        if (!(arg1 && arg2)) {
-            cmd.out = 'Error: needs 2 variables.';
-            return cmd;
-        }
+    main: function() {
+        this.echo('Redirecting...')
+        location.href = 'http://main.readiz.com/';
+    },
+    about: function() {
+        this.echo('Redirecting...')
+        location.href = 'http://about.readiz.com/';
+    },
+    log: function() {
+        this.echo('Redirecting...')
+        location.href = 'https://log.readiz.com/';
+    },
+    blog: function() {
+        this.echo('Redirecting...')
+        location.href = 'http://blog.readiz.com/';
+    },
+    util: function() {
+        this.echo('Redirecting...')
+        location.href = 'http://util.readiz.com/';
+    },
+    feed: function() {
+        this.echo('Redirecting...')
+        location.href = 'http://feed.readiz.com/';
+    },
+    bot: function() {
+        this.echo('Redirecting...')
+        location.href = 'http://bot.readiz.com/';
+    },
+    stock: function() {
+        this.echo('Redirecting...')
+        location.href = 'https://p.readiz.com/stock';
+    },
+    memo: function() {
+        this.echo('Redirecting...')
+        location.href = 'https://p.readiz.com/memo';
+    },
+    hobby: function() {
+        this.echo('Redirecting...')
+        location.href = 'https://p.readiz.com/collected/productive_hobby';
+    },
+    echo: function(arg1) {
+        this.echo(arg1);
+    },
+    // calc: {
+    //     add: function(a, b) {
+    //         this.echo(a+b);
+    //     },
+    //     sub: function(a, b) {
+    //         this.echo(a-b);
+    //     }
+    // },
+    plogin: function(arg1, arg2) {
+        this.echo('Signing in...');
 
         var url = "https://p.readiz.com/do_login";
         var values = { username: arg1, password: arg2, return_page: 'https://p.readiz.com/summary' };
@@ -186,10 +178,17 @@ Readiz.makePrivatePageObject('collected'),
         document.body.appendChild(form);
         form.submit();
         document.body.removeChild(form);
-
-        cmd.out = 'Signing In...'
-        return cmd;
-    },
-    help : 'Private login'
-}
-];
+    }
+};
+Readiz.terminalSettingsObject = { 
+    prompt: 'user@readiz>', 
+    greeting: true,
+    greetings: '#######################################\n' +
+            '# Readiz\n' +
+            '# -\n' +
+            '# Read everything in the world easily.\n' +
+            '# http://readiz.com/\n' +
+            '########################################\n\n' +
+            'Executable commands:\n' +
+            'main, about, log, blog, util, feed, bot, \nstock, memo, hobby, echo, help\n'
+};
