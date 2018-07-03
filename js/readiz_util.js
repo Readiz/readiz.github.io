cd063ee95a8c9e45be88a01327eda1967f8c6b66
unknown <readiz@readiz.com>
2018-07-03 23:29:45 +0900

    Fetch test

--- js/readiz_util.js
+++ js/readiz_util.js
@@ -1,99 +1,3 @@
-var createElement = (function()
-{
-    // Detect IE using conditional compilation
-    if (/*@cc_on @*//*@if (@_win32)!/*@end @*/false)
-    {
-        // Translations for attribute names which IE would otherwise choke on
-        var attrTranslations =
-        {
-            "class": "className",
-            "for": "htmlFor"
-        };
-
-        var setAttribute = function(element, attr, value)
-        {
-            if (attrTranslations.hasOwnProperty(attr))
-            {
-                element[attrTranslations[attr]] = value;
-            }
-            else if (attr == "style")
-            {
-                element.style.cssText = value;
-            }
-            else
-            {
-                element.setAttribute(attr, value);
-            }
-        };
-
-        return function(tagName, attributes)
-        {
-            attributes = attributes || {};
-
-            // See http://channel9.msdn.com/Wiki/InternetExplorerProgrammingBugs
-            if (attributes.hasOwnProperty("name") ||
-                attributes.hasOwnProperty("checked") ||
-                attributes.hasOwnProperty("multiple"))
-            {
-                var tagParts = ["<" + tagName];
-                if (attributes.hasOwnProperty("name"))
-                {
-                    tagParts[tagParts.length] =
-                        ' name="' + attributes.name + '"';
-                    delete attributes.name;
-                }
-                if (attributes.hasOwnProperty("checked") &&
-                    "" + attributes.checked == "true")
-                {
-                    tagParts[tagParts.length] = " checked";
-                    delete attributes.checked;
-                }
-                if (attributes.hasOwnProperty("multiple") &&
-                    "" + attributes.multiple == "true")
-                {
-                    tagParts[tagParts.length] = " multiple";
-                    delete attributes.multiple;
-                }
-                tagParts[tagParts.length] = ">";
-
-                var element =
-                    document.createElement(tagParts.join(""));
-            }
-            else
-            {
-                var element = document.createElement(tagName);
-            }
-
-            for (var attr in attributes)
-            {
-                if (attributes.hasOwnProperty(attr))
-                {
-                    setAttribute(element, attr, attributes[attr]);
-                }
-            }
-
-            return element;
-        };
-    }
-    // All other browsers
-    else
-    {
-        return function(tagName, attributes)
-        {
-            attributes = attributes || {};
-            var element = document.createElement(tagName);
-            for (var attr in attributes)
-            {
-                if (attributes.hasOwnProperty(attr))
-                {
-                    element.setAttribute(attr, attributes[attr]);
-                }
-            }
-            return element;
-        };
-    }
-})();
-
 var Readiz = {};
 Readiz.terminalSettingsObject = { 
     ps: '[user@readiz]$', 
@@ -148,6 +52,17 @@ Readiz.terminalCommandsObject = [{
     name : 'hello',
     method : function(cmd){
         cmd.out = '<h1>Hello World!</h1>';
+        fetch("https://t.readiz.com/menu", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: "Test"
+            }).then(function(res) {
+                alert(JSON.stringify(res));
+            }, function(e) {
+                alert("Error submitting form!");
+        });
         return cmd;
     },
     help : 'A demo command.'
