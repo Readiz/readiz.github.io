e0e123140ba00ef7d8550c18db3bbdc0e4776db7
Readiz <readiz@readiz.com>
2023-04-13 17:06:30 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@5dd0bc6bb9c7d2cd022c521c3d1e2ad022d8ceb5 🚀

---
+++ pages/practice/React/StyleExample$.tsx
@@ -0,0 +1,21 @@
+/**
+ * @title Style Example
+ */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.module.css';
+
+const Page = () => {
+  return (
+    <div className="container mx-auto px-6">
+        <div className="flex flex-row">
+            <div className="basis-1/4 md:basis-1/3">01</div>
+            <div className="basis-1/4 md:basis-1/3">02</div>
+            <div className="basis-1/2 md:basis-1/3">03</div>
+        </div>
+    </div>
+  )
+}
+
+export default Page
