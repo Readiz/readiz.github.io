39c21bdf386eb5fb9e27146edbb93828dcee6f50
Readiz <readiz@readiz.com>
2023-04-14 08:45:49 +0900

    clean doc

--- docs/assets/index_-b9e11e64.js
+++
@@ -1,1 +0,0 @@
-const e=[{depth:1,text:"reference index page",id:"reference-index-page"}],t=Object.freeze(Object.defineProperty({__proto__:null,outline:e},Symbol.toStringTag,{value:"Module"}));export{t as p};
--- docs/assets/index_-beaf3b02.js
+++
@@ -1,1 +0,0 @@
-const e=[{depth:1,text:"Useful Links",id:"useful-links"},{depth:2,text:"Algorithm",id:"algorithm"},{depth:2,text:"Hobby",id:"hobby"}],t=Object.freeze(Object.defineProperty({__proto__:null,outline:e},Symbol.toStringTag,{value:"Module"}));export{t as p};
