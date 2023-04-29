04d934b5286163bd406a466ac6446547ff495986
readiz <readiz@readiz.com>
2023-04-29 21:42:20 +0900

    add PS

---
+++ components/HelloGraph.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'mafs/core.css';
+// import { Mafs, Coordinates, Plot, Theme } from "mafs";
+// import pkg from 'mafs';
+// const { Coordinates, Plot, Mafs, Theme } = pkg;
+
+interface Props {}
+
+const HelloGraph: React.FC<Props> = (props) => {
+  return (
+    <>
+      {/* <Mafs>
+          <Coordinates.Cartesian />
+          <Plot.OfX y={Math.sin} color={Theme.blue} />
+      </Mafs> */}
+    </>
+  )
+}
+
+export default HelloGraph
