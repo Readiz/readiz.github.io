833f4f0ee795073879c86ea4d80b6137435bb6d0
Readiz <readiz@readiz.com>
2023-05-24 09:51:42 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@6f99c772d3b3a1abbfadd5c848158cc157af40ad 🚀

---
+++ assets/BAMusicPlayer-13746a88.js
@@ -0,0 +1,1 @@
+import{j as e}from"./ssg-client-31bc6150.js";import{I as r}from"./IFrame-083d1ca3.js";import"./index.module-caf545e8.js";const t=l=>e(r,{url:"https://www.readiz.com/BA-music-player/",title:"BA Music Player"}),o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a={};a.main=o;export{a as default};
---
+++ assets/RandomGame-6811fb93.js
@@ -0,0 +1,1 @@
+import{j as e}from"./ssg-client-31bc6150.js";import{I as o}from"./IFrame-083d1ca3.js";import"./index.module-caf545e8.js";const t=a=>e(o,{url:"https://www.readiz.com/RandomGame/",title:"Random Game"}),m=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),r={};r.main=m;export{r as default};
