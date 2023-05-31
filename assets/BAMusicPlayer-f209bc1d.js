68e4f7e7fdedbb3433346d01eadbc892d1b8fd4a
Readiz <readiz@readiz.com>
2023-05-31 17:25:44 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@f180315c5d6d0fed6b8bf1528051ada0f7760a9e 🚀

---
+++ assets/BAMusicPlayer-f209bc1d.js
@@ -0,0 +1,1 @@
+import{j as e}from"./ssg-client-e78c6a6f.js";import{I as r}from"./IFrame-a7d9e44e.js";import"./index.module-caf545e8.js";const t=l=>e(r,{url:"https://www.readiz.com/BA-music-player/",title:"BA Music Player"}),o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a={};a.main=o;export{a as default};
