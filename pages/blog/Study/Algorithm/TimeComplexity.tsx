c7d3659df0886ddf17055e53c0de47d429a3ef97
Readiz <readiz@readiz.com>
2023-06-02 07:41:25 +0900

    add time

--- pages/blog/Study/Algorithm/TimeComplexity.tsx
+++ pages/blog/Study/Algorithm/TimeComplexity.tsx
@@ -8,6 +8,7 @@ const TimeComplexity: React.FC<Props> = (props) => {
   const ln = (x: number) => x > 0 ? Math.log2(x + 1): 0
   const xln = (x: number) => x > 0 ? x * Math.log2(x + 1): 0
   const square = (x: number) => x > 0 ? x * x: 0
+  const root = (x: number) => x > 0 ? Math.sqrt(x): 0
   const identity = (x: number) => x > 0 ? x : 0
   return (
     <>
@@ -24,12 +25,14 @@ const TimeComplexity: React.FC<Props> = (props) => {
           />
           <Text x={270} y={200} color={Theme.blue} size={20}>O(n)</Text>
           <Plot.OfX y={identity} color={Theme.blue} />
-          <Text x={400} y={50} color={Theme.pink} size={20}>O(log n)</Text>
+          <Text x={200} y={50} color={Theme.pink} size={20}>O(log n)</Text>
           <Plot.OfX y={ln} color={Theme.pink} />
           <Text x={150} y={300} color={Theme.indigo} size={20}>O(n log n)</Text>
           <Plot.OfX y={xln} color={Theme.indigo} />
           <Text x={80} y={500} color={Theme.green} size={20}>O(n^2)</Text>
           <Plot.OfX y={square} color={Theme.green} />
+          <Text x={350} y={60} color={Theme.orange} size={20}>O(n^0.5)</Text>
+          <Plot.OfX y={root} color={Theme.orange} />
           
       </Mafs>
     </>
