449e47c700805b48b32f233e4a6590152fad3a02
Readiz <readiz@readiz.com>
2023-07-02 12:52:46 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@2db86c45db0ca85cd818033aa98db7513f099d84 🚀

---
+++ assets/link-b917876b.js
@@ -0,0 +1,42 @@
+import{p as c}from"./link_-67adaa6b.js";import{u as h,j as n,a as i,F as o}from"./ssg-client-411e0f30.js";import{H as s}from"./Header-00f59403.js";import"./index.module-caf545e8.js";function l(r){const e=Object.assign({p:"p",h3:"h3",ul:"ul",li:"li",a:"a"},h(),r.components);return i(o,{children:[n(s,{}),`
+`,n(e.p,{children:"개인적으로 보기 위한 와드 모음"}),`
+`,n(e.h3,{id:"personal",children:"Personal"}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:n(e.a,{href:"https://github.com/Readiz",children:"GitHub Profile"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://solved.ac/profile/readiz",children:"Solved.ac Profile"})}),`
+`]}),`
+`,n(e.h3,{id:"developing",children:"Developing"}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:n(e.a,{href:"https://news.hada.io/",children:"GeekNews"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://blog.outsider.ne.kr/",children:"Outsider's Dev Stroy"})}),`
+`]}),`
+`,n(e.h3,{id:"problem-solving",children:"Problem Solving"}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:n(e.a,{href:"https://modoocode.com/",children:"모두의 코드"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://out.swexpertacademy.samsung.com/",children:"SW Expert Acadmey"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://www.acmicpc.net/",children:"BOJ"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://codeforces.com/contests",children:"Codeforces Contests"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://infossm.github.io/blog/",children:"소멤 블로그"})}),`
+`]}),`
+`,n(e.h3,{id:"ps--optimization-related-techniques--site--documents",children:"PS & Optimization related techniques / site / documents"}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:n(e.a,{href:"https://en.algorithmica.org/",children:"Algorithmica"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://algorithm-visualizer.org/",children:"Algorithm Visualizer"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://cp-algorithms.com/",children:"CP Algorithms"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://maspypy.github.io/library/",children:"maspy library"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://github.com/justiceHui/Unknown-To-Wellknown",children:"Wellknown Techniques"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://github.com/koosaga/DeobureoMinkyuParty",children:"더불어민규파티 팀노트"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://github.com/justiceHui/icpc-teamnote/blob/master/main.pdf",children:"JusticeHui 팀노트"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://github.com/koosaga/olympiad",children:"koosaga olympiad"})}),`
+`]}),`
+`,n(e.h3,{id:"hobby",children:"Hobby"}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:n(e.a,{href:"https://tetr.io/",children:"Tetr.io"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://minesweeper.online/",children:"Minesweeper"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://www.chess.com/",children:"Chess"})}),`
+`]}),`
+`,n(e.h3,{id:"work",children:"Work"}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:n(e.a,{href:"https://www.secrbs.net/",children:"RBS"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://www.secdwp.net",children:"RBS 3.0"})}),`
+`]})]})}function d(r={}){const{wrapper:e}=Object.assign({},h(),r.components);return e?n(e,Object.assign({},r,{children:n(l,r)})):l(r)}const a=Object.freeze(Object.defineProperty({__proto__:null,default:d},Symbol.toStringTag,{value:"Module"})),t={};t.outlineInfo=c;t.main=a;export{t as default};
