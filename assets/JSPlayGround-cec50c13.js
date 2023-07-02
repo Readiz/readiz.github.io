6e2ef58254a694295cb85eb00bb075ed39e5a24e
Readiz <readiz@readiz.com>
2023-07-02 13:30:21 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@fadf7bcc06955ea9322ae644368426cd70cd4ade 🚀

---
+++ assets/JSPlayGround-cec50c13.js
@@ -0,0 +1,1 @@
+import{j as o}from"./ssg-client-7c1334ba.js";import{I as r}from"./IFrame-5889a1e6.js";import"./index.module-caf545e8.js";const t=l=>o(r,{url:"https://www.readiz.com/JSPlaygrounds/",title:"JS Playgrounds"}),e=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a={};a.main=e;export{a as default};
---
+++ assets/RandomGame-e069e023.js
@@ -0,0 +1,1 @@
+import{j as e}from"./ssg-client-7c1334ba.js";import{I as o}from"./IFrame-5889a1e6.js";import"./index.module-caf545e8.js";const t=a=>e(o,{url:"https://www.readiz.com/RandomGame/",title:"Random Game"}),m=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),r={};r.main=m;export{r as default};
