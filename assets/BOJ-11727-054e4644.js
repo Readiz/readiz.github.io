509ae4062dc501e58abd9da0235c983a6fd97cb8
Readiz <readiz@readiz.com>
2023-07-16 04:46:09 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@2319d10b953f9dc6439d989e2c685327dce63e13 🚀

---
+++ assets/BOJ-11727-054e4644.js
@@ -0,0 +1,30 @@
+import{p as a}from"./BOJ-11727_-79628163.js";import{u as c,j as e,a as i,F as s}from"./ssg-client-139670a6.js";import{C as l}from"./Comment-b720a249.js";import{H as m}from"./Header-7048dc36.js";import{T as p}from"./TagList-c7ae6184.js";import"./index.module-caf545e8.js";function r(t){const n=Object.assign({ul:"ul",li:"li",a:"a",p:"p",pre:"pre",code:"code"},c(),t.components);return i(s,{children:[e(m,{}),`
+`,i(n.ul,{children:[`
+`,e(n.li,{children:"문제 제목: 2×n 타일링 2"}),`
+`,i(n.li,{children:["문제 링크: ",e(n.a,{href:"https://www.acmicpc.net/problem/11727",children:"https://www.acmicpc.net/problem/11727"})]}),`
+`]}),`
+`,e(n.p,{children:"간단한 DP 연습 문제. 아래 점화식을 N = 3, 4, 5 반복해보면 관찰할 수 있다."}),`
+`,e(n.pre,{children:e(n.code,{children:`DP[i] = DP[i-1] + DP[i-2] * 2
+`})}),`
+`,e(n.p,{children:"N 은 1000보다 작은 정수이므로 위 점화식으로 시간 복잡도 안으로 구하기 가능. 끝."}),`
+`,e(n.pre,{children:e(n.code,{className:"language-cpp",children:`#include <bits/stdc++.h>
+using namespace std;
+
+int DP[1001] = {0, 1, 3};
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for(int i = 3; i <= N; ++i) {
+        DP[i] = DP[i-1] + (DP[i-2] << 1);
+        DP[i] %= 10007;
+    }
+
+    printf("%d\\n", DP[N]);
+
+    return 0;
+}
+`})}),`
+`,e(p,{}),`
+`,e(l,{})]})}function d(t={}){const{wrapper:n}=Object.assign({},c(),t.components);return n?e(n,Object.assign({},t,{children:e(r,t)})):r(t)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:d},Symbol.toStringTag,{value:"Module"})),o={};o.outlineInfo=a;o.main=u;export{o as default};
---
+++ assets/MacTips-0db381a5.js
@@ -0,0 +1,18 @@
+import{p as c}from"./MacTips_-1b59d553.js";import{u as r,j as e,a as o,F as a}from"./ssg-client-139670a6.js";import{C as m}from"./Comment-b720a249.js";import{H as s}from"./Header-7048dc36.js";import{T as d}from"./TagList-c7ae6184.js";import"./katex.min-4ed993c7.js";import"./react-katex-2b8bdcdb.js";import"./index.module-caf545e8.js";function i(t){const n=Object.assign({h1:"h1",p:"p",h2:"h2",ul:"ul",li:"li"},r(),t.components);return o(a,{children:[e(s,{}),`
+`,e(n.h1,{id:"나만-보는-mac-팁들",children:"나만 보는 Mac 팁들"}),`
+`,e(n.p,{children:"어언 나도 Mac 유저 2년차이다. 자꾸 기억을 까먹는 Mac 팁들을 정리한다."}),`
+`,e(n.h2,{id:"스크린샷-단축키",children:"스크린샷 단축키"}),`
+`,o(n.ul,{children:[`
+`,o(n.li,{children:["Cmd + Control + Shift + 3",`
+`,o(n.ul,{children:[`
+`,e(n.li,{children:"화면 전체를 캡쳐 + 클립보드로"}),`
+`]}),`
+`]}),`
+`,o(n.li,{children:["Cmd + Control + Shift + 4",`
+`,o(n.ul,{children:[`
+`,e(n.li,{children:"영역을 지정해서 캡쳐 + 클립보드로"}),`
+`]}),`
+`]}),`
+`]}),`
+`,e(d,{}),`
+`,e(m,{})]})}function h(t={}){const{wrapper:n}=Object.assign({},r(),t.components);return n?e(n,Object.assign({},t,{children:e(i,t)})):i(t)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:h},Symbol.toStringTag,{value:"Module"})),l={};l.outlineInfo=c;l.main=u;export{l as default};
