c6e74f1498eb2a8dfa93a33ee6f1adb3e1e5f956
Readiz <readiz@readiz.com>
2023-07-18 07:26:14 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@e5edc597ece9117083b28ab0b97bf600572223db 🚀

---
+++ assets/MacTips-870eb073.js
@@ -0,0 +1,18 @@
+import{p as c}from"./MacTips_-1b59d553.js";import{u as r,j as e,a as o,F as a}from"./ssg-client-f30b5b8d.js";import{C as m}from"./Comment-58b31db1.js";import{H as s}from"./Header-9436d5df.js";import{T as d}from"./TagList-099ecfa0.js";import"./katex.min-4ed993c7.js";import"./react-katex-f51cacb0.js";import"./index.module-caf545e8.js";function i(t){const n=Object.assign({h1:"h1",p:"p",h2:"h2",ul:"ul",li:"li"},r(),t.components);return o(a,{children:[e(s,{}),`
+`,e(n.h1,{id:"나만-보는-mac-팁들",children:"나만 보는 Mac 팁들"}),`
+`,e(n.p,{children:"어언 나도 Mac 유저 2년차이다. 자꾸 기억을 까먹는 Mac 팁들을 정리한다."}),`
+`,e(n.h2,{id:"스크린샷-단축키",children:"스크린샷 단축키"}),`
+`,o(n.ul,{children:[`
+`,o(n.li,{children:["Cmd + Control + Shift + 3",`
+`,o(n.ul,{children:[`
+`,e(n.li,{children:"화면 전체를 캡쳐 + 클립보드로"}),`
+`]}),`
+`]}),`
+`,o(n.li,{children:["Cmd + Control + Shift + 4",`
+`,o(n.ul,{children:[`
+`,e(n.li,{children:"영역을 지정해서 캡쳐 + 클립보드로"}),`
+`]}),`
+`]}),`
+`]}),`
+`,e(d,{}),`
+`,e(m,{})]})}function h(t={}){const{wrapper:n}=Object.assign({},r(),t.components);return n?e(n,Object.assign({},t,{children:e(i,t)})):i(t)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:h},Symbol.toStringTag,{value:"Module"})),l={};l.outlineInfo=c;l.main=u;export{l as default};
---
+++ assets/Radix-3d2aa493.js
@@ -0,0 +1,5 @@
+import{p as c}from"./Radix_-89bb5e52.js";import{u as s,j as e,a as o,F as i}from"./ssg-client-f30b5b8d.js";import{C as m}from"./Comment-58b31db1.js";import{H as d}from"./Header-9436d5df.js";import{T as u}from"./TagList-099ecfa0.js";import"./index.module-caf545e8.js";function r(n){const t=Object.assign({p:"p",code:"code",h3:"h3"},s(),n.components);return o(i,{children:[e(d,{}),`
+`,o(t.p,{children:["진법에 관해 정리해본다. 2진법은 특히 ",e(t.code,{children:"Computer Science"}),"와 관련성이 높다."]}),`
+`,e(t.h3,{id:"작성-중",children:"작성 중"}),`
+`,e(u,{}),`
+`,e(m,{})]})}function l(n={}){const{wrapper:t}=Object.assign({},s(),n.components);return t?e(t,Object.assign({},n,{children:e(r,n)})):r(n)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:l},Symbol.toStringTag,{value:"Module"})),a={};a.outlineInfo=c;a.main=p;export{a as default};
