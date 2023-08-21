d2a04b43fd9a17fd5a520ea829f1e146d89aa8f7
Readiz <readiz@readiz.com>
2023-08-21 05:57:32 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@c1ecdfa76df092bd883146f8d681adcbff04f015 🚀

---
+++ assets/BAMusicPlayer-0d665c24.js
@@ -0,0 +1,1 @@
+import{j as e}from"./ssg-client-512f7d75.js";import{I as r}from"./IFrame-bbb81664.js";import"./index.module-caf545e8.js";const t=l=>e(r,{url:"https://www.readiz.com/BA-music-player/",title:"BA Music Player"}),o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a={};a.main=o;export{a as default};
---
+++ assets/Math-f189dbde.js
@@ -0,0 +1,3 @@
+import{p as a}from"./index_-b6de85a6.js";import{u as s,j as t,a as i,F as c}from"./ssg-client-512f7d75.js";import{H as m}from"./Header-6774ec56.js";import{R as l}from"./RelatedWritings-f49d3de5.js";import"./index.module-caf545e8.js";function o(e){const n=Object.assign({h2:"h2"},s(),e.components);return i(c,{children:[t(m,{}),`
+`,t(n.h2,{id:"related-contents",children:"Related Contents"}),`
+`,t(l,{})]})}function u(e={}){const{wrapper:n}=Object.assign({},s(),e.components);return n?t(n,Object.assign({},e,{children:t(o,e)})):o(e)}const d=Object.freeze(Object.defineProperty({__proto__:null,default:u},Symbol.toStringTag,{value:"Module"})),r={};r.outlineInfo=a;r.main=d;export{r as default};
