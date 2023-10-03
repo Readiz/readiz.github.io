0966dfd9c4e252d82649f5a85d7950ec08a8c41f
Readiz <readiz@readiz.com>
2023-10-03 02:48:13 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@a31ab5295c7660c33aead0b405e1f3e2ebcd61ed 🚀

---
+++ assets/ssrPlugin-db21c45b.js
@@ -0,0 +1,1 @@
+import{e as t,R as r,S as c,f as s}from"./ssg-client-a86a4827.js";var p={id:"vite-pages-theme-doc-antd-ssr",prepare(a){const e=t();return{app:r.createElement(c,{cache:e},a),extractStyle:()=>s(e)}}};export{p as default};
