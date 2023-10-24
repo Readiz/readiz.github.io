d9ec8273eb1dc30500f2cdc323efb67ae4db177e
Readiz <readiz@readiz.com>
2023-10-24 14:13:03 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@c732375c1220cab6a74ba4e61d44530d4128c625 🚀

---
+++ assets/Basic-d5e22a00.js
@@ -0,0 +1,11 @@
+import{p as l}from"./Basic_-dd2f6048.js";import{u as c,j as n,a as o,F as a}from"./ssg-client-454411a8.js";import{C as m}from"./Comment-95c3bf78.js";import"./index.module-caf545e8.js";import{H as s}from"./Header-2b94212c.js";import"./katex.min-4ed993c7.js";function i(t){const e=Object.assign({h2:"h2",pre:"pre",code:"code",ul:"ul",li:"li"},c(),t.components);return o(a,{children:[n(s,{}),`
+`,n(e.h2,{id:"gcd",children:"GCD"}),`
+`,n(e.pre,{children:n(e.code,{className:"language-cpp"})}),`
+`,n(e.h2,{id:"lcm",children:"LCM"}),`
+`,n(e.pre,{children:n(e.code,{className:"language-cpp"})}),`
+`,n(e.h2,{id:"time-complexity",children:"Time Complexity"}),`
+`,o(e.ul,{children:[`
+`,n(e.li,{children:"GCD:"}),`
+`,n(e.li,{children:"LCM:"}),`
+`]}),`
+`,n(m,{})]})}function d(t={}){const{wrapper:e}=Object.assign({},c(),t.components);return e?n(e,Object.assign({},t,{children:n(i,t)})):i(t)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:d},Symbol.toStringTag,{value:"Module"})),r={};r.outlineInfo=l;r.main=p;export{r as default};
---
+++ assets/TIS-100-4fa22670.js
@@ -0,0 +1,5 @@
+import{p as s}from"./TIS-100_-79628163.js";import{u as a,j as n,a as m,F as i}from"./ssg-client-454411a8.js";import{C as c}from"./Comment-95c3bf78.js";import{H as p}from"./Header-2b94212c.js";import{T as u}from"./TagList-10b3f4e2.js";import{I as l}from"./Image-33c84315.js";import"./index.module-caf545e8.js";function o(t){const e=Object.assign({p:"p"},a(),t.components);return m(i,{children:[n(p,{}),`
+`,n(e.p,{children:"TIS-100 Manual"}),`
+`,n(l,{src:"2023-06-19-22-29-56.png"}),`
+`,n(u,{}),`
+`,n(c,{})]})}function f(t={}){const{wrapper:e}=Object.assign({},a(),t.components);return e?n(e,Object.assign({},t,{children:n(o,t)})):o(t)}const d=Object.freeze(Object.defineProperty({__proto__:null,default:f},Symbol.toStringTag,{value:"Module"})),r={};r.outlineInfo=s;r.main=d;export{r as default};
