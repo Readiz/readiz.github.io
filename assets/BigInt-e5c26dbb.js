1a4a72080fab73112116e64ae821fc83e89b4f39
Readiz <readiz@readiz.com>
2023-10-25 05:54:47 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@077f8cca00d551fdb5a7b8382ea546b1b4e1dea9 🚀

---
+++ assets/BigInt-e5c26dbb.js
@@ -0,0 +1,14 @@
+import{p as a}from"./BigInt_-938af11c.js";import{u as r,j as e,a as i,F as s}from"./ssg-client-a0ffd2b3.js";import{C as l}from"./Comment-0b818857.js";import"./index.module-caf545e8.js";import{H as m}from"./Header-b47fa9dd.js";import"./katex.min-4ed993c7.js";function o(t){const n=Object.assign({h2:"h2",pre:"pre",code:"code",ul:"ul",li:"li"},r(),t.components);return i(s,{children:[e(m,{}),`
+`,e(n.h2,{id:"bigint-basic",children:"BigInt Basic"}),`
+`,e(n.pre,{children:e(n.code,{className:"language-cpp",children:`struct BigInt {
+    // add
+    // subtract
+    // multiply
+    // div (?)
+};
+`})}),`
+`,e(n.h2,{id:"time-complexity",children:"Time Complexity"}),`
+`,i(n.ul,{children:[`
+`,e(n.li,{}),`
+`]}),`
+`,e(l,{})]})}function d(t={}){const{wrapper:n}=Object.assign({},r(),t.components);return n?e(n,Object.assign({},t,{children:e(o,t)})):o(t)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:d},Symbol.toStringTag,{value:"Module"})),c={};c.outlineInfo=a;c.main=u;export{c as default};
