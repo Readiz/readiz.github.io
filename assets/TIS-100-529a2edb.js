f6578a48ef21cba869dfc48dba7c406db13c187b
Readiz <readiz@readiz.com>
2023-10-25 06:03:46 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@6684624c72c3c01d37199e821bd3cd0bf86a3a54 🚀

---
+++ assets/TIS-100-529a2edb.js
@@ -0,0 +1,5 @@
+import{p as s}from"./TIS-100_-79628163.js";import{u as a,j as n,a as m,F as i}from"./ssg-client-fe6a1422.js";import{C as c}from"./Comment-2eab8abc.js";import{H as p}from"./Header-11e5d9ae.js";import{T as u}from"./TagList-6c8837f0.js";import{I as l}from"./Image-17d7af43.js";import"./index.module-caf545e8.js";function o(t){const e=Object.assign({p:"p"},a(),t.components);return m(i,{children:[n(p,{}),`
+`,n(e.p,{children:"TIS-100 Manual"}),`
+`,n(l,{src:"2023-06-19-22-29-56.png"}),`
+`,n(u,{}),`
+`,n(c,{})]})}function f(t={}){const{wrapper:e}=Object.assign({},a(),t.components);return e?n(e,Object.assign({},t,{children:n(o,t)})):o(t)}const d=Object.freeze(Object.defineProperty({__proto__:null,default:f},Symbol.toStringTag,{value:"Module"})),r={};r.outlineInfo=s;r.main=d;export{r as default};
