692b003279c0801c97a1688127cd73f99dcb3f8a
Readiz <readiz@readiz.com>
2023-11-14 16:01:48 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@09496f5e63e850ab48732ea34f6867e8c62c5ea7 🚀

--- assets/RandomGame-c03aa592.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./ssg-client-8fb99429.js";import{I as o}from"./IFrame-95f47007.js";import"./index.module-caf545e8.js";const t=a=>e(o,{url:"https://www.readiz.com/RandomGame/",title:"Random Game"}),m=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),r={};r.main=m;export{r as default};
