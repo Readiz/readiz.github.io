c92eaddabc3aad5650ad8320dce7b47cf4fc1489
Readiz <readiz@readiz.com>
2023-11-14 15:26:19 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@e3e55b994a2316c534c61b68b3ec8de3f713cba5 🚀

---
+++ assets/BAMusicPlayer-88916fc3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./ssg-client-673a2109.js";import{I as r}from"./IFrame-fcf1809d.js";import"./index.module-caf545e8.js";const t=l=>e(r,{url:"https://www.readiz.com/BA-music-player/",title:"BA Music Player"}),o=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a={};a.main=o;export{a as default};
---
+++ assets/BOJ-11057-41f46eae.js
@@ -0,0 +1,38 @@
+import{p as a}from"./BOJ-11057_-79628163.js";import{u as o,j as e,a as i,F as s}from"./ssg-client-673a2109.js";import{C as l}from"./Comment-c2ecb3a1.js";import{H as p}from"./Header-c1c09d5e.js";import{T as d}from"./TagList-a809f8fa.js";import"./index.module-caf545e8.js";function r(t){const n=Object.assign({ul:"ul",li:"li",a:"a",p:"p",pre:"pre",code:"code"},o(),t.components);return i(s,{children:[e(p,{}),`
+`,i(n.ul,{children:[`
+`,e(n.li,{children:"문제 제목: 오르막 수"}),`
+`,i(n.li,{children:["문제 링크: ",e(n.a,{href:"https://www.acmicpc.net/problem/11057",children:"https://www.acmicpc.net/problem/11057"})]}),`
+`]}),`
+`,e(n.p,{children:"간단한 DP 연습 문제. 오르막 수는 n번째 자리에 올 수 있는 수가 n - 1번째 자리에 올 수 있는 수로 정해진다."}),`
+`,e(n.p,{children:"k라는 수가 n번째 자리에 올 수 있는 경우의 수는 n - 1번째 자리에서 0 ~ k까지 올 수 있는 수의 합이 된다."}),`
+`,e(n.pre,{children:e(n.code,{children:`DP[k][n] = DP[k][n-1] + DP[k-1][n-1] + ... DP[0][n-1]
+`})}),`
+`,e(n.p,{children:"경우의 수를 셀 때에는 0번째 위치의 경우의 수를 1로 초기화해서 풀어야 함에 유의."}),`
+`,e(n.pre,{children:e(n.code,{className:"language-cpp",children:`#include <bits/stdc++.h>
+using namespace std;
+
+int DP[10][1001];
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for(int i = 0; i < 10; ++i) {
+        DP[i][0] = 1;
+    }
+
+    for(int n = 1; n <= N; ++n) {
+        for(int K = 0; K < 10; ++K) { // n번째 수가 K일 때 수 세기
+            int& T = DP[K][n];
+            for(int k = 0; k <= K; ++k) {
+                T += DP[k][n - 1];
+            }
+            T %= 10007;
+        }
+    }
+
+    printf("%d\\n", DP[9][N]);
+    return 0;
+}
+`})}),`
+`,e(d,{}),`
+`,e(l,{})]})}function m(t={}){const{wrapper:n}=Object.assign({},o(),t.components);return n?e(n,Object.assign({},t,{children:e(r,t)})):r(t)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:m},Symbol.toStringTag,{value:"Module"})),c={};c.outlineInfo=a;c.main=u;export{c as default};
