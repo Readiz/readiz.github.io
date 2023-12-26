8ca4f82f2bb3bea8b75fe66c9d0184d65b0dd2a0
Readiz <readiz@readiz.com>
2023-12-26 14:51:43 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@110affc6f43cc7d6fa21759061519cb25c31ca4d 🚀

--- assets/BOJ-95a387de.js
+++
@@ -1,5 +0,0 @@
-import{p as a}from"./index_-6c227933.js";import{u as r,j as e,a as i,F as c}from"./ssg-client-0782e3c4.js";import{H as l}from"./Header-83e60b87.js";import{R as m}from"./RelatedWritings-969917f9.js";import"./index.module-caf545e8.js";function o(n){const t=Object.assign({h2:"h2",p:"p"},r(),n.components);return i(c,{children:[e(l,{}),`
-`,e(t.h2,{id:"boj",children:"BOJ?"}),`
-`,e(t.p,{children:"Baekjoon Online Judge의 약자."}),`
-`,e(t.h2,{id:"related-contents",children:"Related Contents"}),`
-`,e(m,{})]})}function d(n={}){const{wrapper:t}=Object.assign({},r(),n.components);return t?e(t,Object.assign({},n,{children:e(o,n)})):o(n)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:d},Symbol.toStringTag,{value:"Module"})),s={};s.outlineInfo=a;s.main=u;export{s as default};
---
+++ assets/index__-68e6700f.js
@@ -0,0 +1,39 @@
+import{p as a}from"./index_-79628163.js";import{u as l,j as e,a as r,F as d,L as t}from"./ssg-client-977b0f15.js";import{I as h}from"./Image-7167ed49.js";import{R as m}from"./RecentWritings-8928a34f.js";import{T as u}from"./TagSummary-6df31c06.js";import{H as p}from"./Header-5c8811ee.js";import"./index.module-caf545e8.js";function c(i){const n=Object.assign({h2:"h2",p:"p",em:"em",ul:"ul",li:"li"},l(),i.components);return r(d,{children:[e(n.h2,{id:"symbol",children:"Symbol"}),`
+`,e(h,{src:"readiz.jpg",width:"100"}),`
+`,e(n.h2,{id:"소개",children:"소개"}),`
+`,e(n.p,{children:e(n.em,{children:"기록은 인류를 진화할 수 있게 만든 위대한 유산이다."})}),`
+`,e(n.p,{children:"글들은 Markdown을 확장한 MDX 기반으로 작성되고 있습니다."}),`
+`,e(n.p,{children:"주로 개발 / 컴퓨터와 관련된 이야기를 다룹니다. 개인이 적은 글이다 보니 언제든지 틀린 점이 있을 수 있다는 것 유의해주세요."}),`
+`,e(n.h2,{id:"primary-index",children:"Primary Index"}),`
+`,r(n.ul,{children:[`
+`,r(n.li,{children:[`
+`,e(t,{to:"/blog",children:"Blog"}),`
+`,r(n.ul,{children:[`
+`,e(n.li,{children:"주로 개발과 관련된 글들을 올립니다."}),`
+`]}),`
+`]}),`
+`,r(n.li,{children:[`
+`,e(t,{to:"/ps",children:"PS Snippet"}),`
+`,r(n.ul,{children:[`
+`,e(n.li,{children:"PS(Problem Solving)에서 유용하게 쓰일 수 있는 코드 조각이나 정보 모음입니다."}),`
+`]}),`
+`]}),`
+`,r(n.li,{children:[`
+`,e(t,{to:"/showcase",children:"Showcase"}),`
+`,r(n.ul,{children:[`
+`,e(n.li,{children:"Readiz가 만든 간단한 작업물들을 볼 수 있습니다. 2023년부터는 과거 작업물들을 더이상 잃지 않겠다는 생각으로 시작했습니다."}),`
+`]}),`
+`]}),`
+`,r(n.li,{children:["Private Space (Under Construction)",`
+`,r(n.ul,{children:[`
+`,e(n.li,{children:"공개되지 않거나 제한적으로 공개된 공간입니다."}),`
+`]}),`
+`]}),`
+`]}),`
+`,e(n.h2,{id:"recent-writings",children:"Recent Writings"}),`
+`,e(n.p,{children:"최근엔 아래와 같은 글들이 업데이트 되었습니다. 최대 10건이 노출됩니다. 전체 리스트는 Blog 탭을 참고해주세요."}),`
+`,e(m,{}),`
+`,e(n.h2,{id:"주제-리스트",children:"주제 리스트"}),`
+`,e(n.p,{children:"아래에 다루고 있는 주제별로 글 목록을 확인할 수 있습니다."}),`
+`,e(u,{})]})}function g(i={}){const{wrapper:n}=Object.assign({},l(),i.components);return n?e(n,Object.assign({},i,{children:e(c,i)})):c(i)}function o(i){return r(d,{children:[e(p,{title:"Welcome"}),`
+`,e(g,{})]})}function f(i={}){const{wrapper:n}=Object.assign({},l(),i.components);return n?e(n,Object.assign({},i,{children:e(o,i)})):o()}const b=Object.freeze(Object.defineProperty({__proto__:null,default:f},Symbol.toStringTag,{value:"Module"})),s={};s.outlineInfo=a;s.main=b;export{s as default};
