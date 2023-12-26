a1458cd2c5d2c7e1883172e1fe626bcac3d08ba3
Readiz <readiz@readiz.com>
2023-12-26 14:53:45 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@254ccfd15a1cb74d2568a18937cb486f5ea1acc3 🚀

--- assets/EndGame-744a4152.js
+++
@@ -1,7 +0,0 @@
-import{p as i}from"./EndGame_-f6bd1a48.js";import{u as r,j as n,a as m,F as s}from"./ssg-client-977b0f15.js";import{C as c}from"./Comment-2f5b3023.js";import{H as d}from"./Header-5c8811ee.js";import{T as l}from"./TagList-e85fd401.js";import"./index.module-caf545e8.js";function o(t){const e=Object.assign({h2:"h2",p:"p"},r(),t.components);return m(s,{children:[n(d,{}),`
-`,n(e.h2,{id:"im-in-the-endgame-now",children:"I'm in the endgame now(?)"}),`
-`,n(e.p,{children:"갑자기 심경변화가 와서 나중에 언제 이런 결정을 내렸나.. 찾기 위해서 글로 한자 남겨둔다."}),`
-`,n(e.p,{children:"몇 개 남지 않았던 모바일 게임을 하는 것을 그만두었다. 시간 소모도 소모이지만, 다른 것보다도 모바일 게임이 이제 즐거움을 주기보다 스트레스를 더 많이 줘서가 다 계정 삭제를 때렸다. 캐주얼한 게임은 앞으로도 가끔씩은 즐길지도 모르겠지만, 출첵 요구를 한다던지 하는 게임은 앞으로 하지 않을 듯.."}),`
-`,n(e.p,{children:"대신에 다른 건전한 취미생활을 늘려나가도록 하자."}),`
-`,n(l,{}),`
-`,n(c,{})]})}function p(t={}){const{wrapper:e}=Object.assign({},r(),t.components);return e?n(e,Object.assign({},t,{children:n(o,t)})):o(t)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:p},Symbol.toStringTag,{value:"Module"})),a={};a.outlineInfo=i;a.main=u;export{a as default};
---
+++ assets/ssrPlugin-321fc21c.js
@@ -0,0 +1,1 @@
+import{e as t,R as r,S as c,f as s}from"./ssg-client-104e7b0e.js";var p={id:"vite-pages-theme-doc-antd-ssr",prepare(a){const e=t();return{app:r.createElement(c,{cache:e},a),extractStyle:()=>s(e)}}};export{p as default};
