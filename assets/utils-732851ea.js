17d0faee5cfde5342765573df93c4d2c71586784
Readiz <readiz@readiz.com>
2024-02-06 14:57:45 +0000

    Deploying to gh-pages from @ Readiz/readiz.github.io@df388f88afe65812187b73990491d06a9f1a459a 🚀

---
+++ assets/utils-732851ea.js
@@ -0,0 +1,9 @@
+import{p as s}from"./utils_-c697292c.js";import{u as i,j as n,a as t,F as a}from"./ssg-client-7a654c7b.js";import{H as c}from"./Header-31f4d24d.js";import"./index.module-caf545e8.js";function r(o){const e=Object.assign({p:"p",h3:"h3",ul:"ul",li:"li",a:"a"},i(),o.components);return t(a,{children:[n(c,{}),`
+`,n(e.p,{children:"Utils"}),`
+`,n(e.h3,{id:"personal",children:"Personal"}),`
+`,n(e.h3,{id:"developing",children:"Developing"}),`
+`,t(e.ul,{children:[`
+`,n(e.li,{children:n(e.a,{href:"https://aka.ms/vs/15/release/vs_WDExpress.exe",children:"VS2017 Express"})}),`
+`]}),`
+`,n(e.h3,{id:"problem-solving",children:"Problem Solving"}),`
+`,n(e.h3,{id:"hobby",children:"Hobby"})]})}function d(o={}){const{wrapper:e}=Object.assign({},i(),o.components);return e?n(e,Object.assign({},o,{children:n(r,o)})):r(o)}const h=Object.freeze(Object.defineProperty({__proto__:null,default:d},Symbol.toStringTag,{value:"Module"})),l={};l.outlineInfo=s;l.main=h;export{l as default};
